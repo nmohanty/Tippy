7558b3d6cad84b58502c828619b23531e0c778b9
nmohanty <Namrata@Sake.local>
2016-09-27 11:47:15 -0700

    added optional animations to the basic tip calculator

--- tipcalc/ViewController.swift
+++ tipcalc/ViewController.swift
@@ -10,10 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var numentered: String!
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var billcell: UITextField!
+    @IBOutlet weak var secondView: UIView!
+    @IBOutlet weak var countControl: UISegmentedControl!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        firstView.alpha = 1.0
+        secondView.alpha = 0.0
+        self.navigationItem.title = "TIPPY"
+        billcell.becomeFirstResponder()
     }
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -21,5 +37,60 @@ class ViewController: UIViewController {
     }
 
 
-}
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+        }
+    
+    
+    @IBAction func onEditchanged(sender: AnyObject) {
+        var numentered: Double {
+            get {
+                return ((billcell.text ?? "0") as NSString).doubleValue
+            }
+            set {
+                billcell.text = String("%f", newValue)
+            }
+        }
+        billField.text = String (format: "%g", numentered)
+
+        
+        firstView.alpha = 0.0
+        billField.becomeFirstResponder()
+        var origin = secondView.frame.origin
+        
+        
+        UIView.animateWithDuration(0.3) {
+            origin.y -= 190
+            self.secondView.alpha = 1.0
+            self.secondView.frame.origin = origin
+            }
+    }
+    
+    @IBAction func onTouch(sender: AnyObject) {
+        print("\(__FUNCTION__) on change happened.")
+        firstView.alpha = 0.0
+        var origin = secondView.frame.origin
+       
+        UIView.animateWithDuration(0.3) {
+            origin.y -= 190
+            self.billField.becomeFirstResponder()
+            self.secondView.alpha = 1.0
+            self.secondView.frame.origin = origin
+        }
+    }
+    
+    
+    @IBAction func calculateTip(sender: AnyObject) {
+        let tipCount:[Double] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        let tipPercentages = [0.18, 0.2, 0.25]
+
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = tip + bill
+        let share = total / tipCount[countControl.selectedSegmentIndex]
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", share)
+        }
+    }
 
